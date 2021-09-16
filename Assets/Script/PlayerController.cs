e987adf65049331f5429e163c5f029d2d6b23314
Can <UC.Ugurlu@student.han.nl>
2021-09-16 10:28:40 +0200

    Added moving and jumping ball

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public Vector3 jump;
+    public bool isGrounded;
+
+    private float jumpForce = 2f;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        isGrounded = true;
+    }
+
+    void OnMove(InputValue movementValue)  
+    { 
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+
+    void FixedUpdate() 
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnCollisionStay(){
+        isGrounded = true;
+    }
+
+    void OnJump()
+    {
+        if(isGrounded){
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
